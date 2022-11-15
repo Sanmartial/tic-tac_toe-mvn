ec10ae97f3b72990b7f19fcc8eb14efe65c59f7e
Roman Globa <globaroman@gmail.com>
2022-11-15 10:29:35 +0200

    Simplify Game class

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,16 +1,17 @@
 import component.*;
 import component.keypad.TerminalNumericKeypadCellNumberConverter;
+import model.Player;
 
 public final class Launcher {
     public static void main(String[] args) {
         final CellNumberConverter cellNumberConverter = new TerminalNumericKeypadCellNumberConverter();
-        //final //CellNumberConverter cellNumberConverter = new DescktopNumericKKeypadCellNumberConverter();
+
         final Game game = new Game(
                 new DataPrinter(cellNumberConverter),
-                new ComputerMove(),
-                new UserMove(cellNumberConverter),
+                new Player(Sign.X, new UserMove(cellNumberConverter)),
+                new Player(Sign.O, new ComputerMove()),
                 new WinnerVirifier(),
-                new CellVerifier());
+                new CellVerifier(), false);
         game.play();
     }
 
--- src/main/java/component/Game.java
+++ src/main/java/component/Game.java
@@ -7,33 +7,34 @@ import java.util.Random;
 
 public class Game {
     private final DataPrinter dataPrinter;
-    private final ComputerMove computerMove;
-    private final UserMove userMove;
+
+    private final Player player1;
+    private final Player player2;
+//    private final ComputerMove computerMove;
+//    private final UserMove userMove;
     private final WinnerVirifier winnerVirifier;
     private final CellVerifier cellVerifier;
 
-    public Game(final DataPrinter dataPrinter,
-                final ComputerMove computerMove,
-                final UserMove userMove,
-                final WinnerVirifier winnerVirifier,
-                final CellVerifier cellVerifier) {
+    private final boolean canSecondlayerMakeFirstMove;
+    public Game(DataPrinter dataPrinter, Player player1, Player player2, WinnerVirifier winnerVirifier, CellVerifier cellVerifier, boolean canSecondlayerMakeFirstMove) {
         this.dataPrinter = dataPrinter;
-        this.computerMove = computerMove;
-        this.userMove = userMove;
+        this.player1 = player1;
+        this.player2 = player2;
         this.winnerVirifier = winnerVirifier;
         this.cellVerifier = cellVerifier;
+        this.canSecondlayerMakeFirstMove = canSecondlayerMakeFirstMove;
     }
 
     public void play() {
         System.out.println("Use the following mapping table to specify a cell using numbers from 1 to 9");
         dataPrinter.printMappingTable(); //display the grid on the monitor
         final GameTable gameTable = new GameTable(); // object game field
-//        if (new Random().nextBoolean()) { //determining who takes the next step
-//            computerMove.make(gameTable); // if True the next step does computer
-//            dataPrinter.printGameTable(gameTable); //display current gaming field
-//        }
+        if (canSecondlayerMakeFirstMove && new Random().nextBoolean()) { //determining who takes the next step
+            player2.makeMove(gameTable); // if True the next step does computer
+            dataPrinter.printGameTable(gameTable); //display current gaming field
+        }
 
-        final Player[] players = new Player[]{new Player(Sign.X, userMove), new Player(Sign.O, computerMove)};
+        final Player[] players = new Player[]{player1, player2};
         while (true) {
 
             for (final Player player : players) {
@@ -45,7 +46,6 @@ public class Game {
                     printGameOver();
                     return;
                 }
-
                 if (cellVerifier.allCellFilled(gameTable)) { //check the draw
                     System.out.println("SORRY, DRAW");
                     printGameOver();
